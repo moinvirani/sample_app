8102087e60803aaaeccf7ac3a801de2154df46c0
Moin Virani <moin.virani@gmail.com>
2013-11-03 22:38:42 -0500

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
-module ApplicationHelper
+module ApplicationHelper	
 
+	#Modules give us a way to package together related methods
 	#Returns the full title on a per-page basis.
 
 	def full_title(page_title)
@@ -11,3 +12,7 @@ module ApplicationHelper
 		end
 	end
 end
+
+
+
+
