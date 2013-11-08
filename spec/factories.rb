1bfc859a40a1c3210439427f075be99f77be2e26
Moin Virani <moin.virani@gmail.com>
2013-11-07 23:21:40 -0500

    Currently at chpt7 listing 7.8, spork is giving 2 errors in user_pages_spec, continue tomorrow

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name 					"Moin Virani"
+		email 					"moin.virani@gmail.com"
+		password				"foobar"
+		password_confirmation 	"foobar"	
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,12 +3,19 @@ require 'spec_helper'
 describe "UserPages" do
 
 	subject {page}
+    
+    describe "profile page" do
+    	let(:user) { FactoryGirl.create(:user)  }
+    	before {visit signup_path}
 
-	describe "signup page" do
+    	it { should have_content(user.name)}
+    	it { should have_title(user.name)}
+    end
+
+    describe "signup page" do
 		before {visit signup_path}
 
 		it { should have_content('Sign up')}
 		it { should have_title(full_title'Sign up')}
 	end
-    
 end
